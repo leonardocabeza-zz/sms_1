4061df14ccd752fb6dfd8deb066cba0b44b39a08
Leonardo Cabeza <cabeza.leo@gmail.com>
2014-07-08 16:42:30 -0430

    - Added notifications for delivered receiver - Removed Toasts for delivered receiver

--- app/src/main/java/receivers/DeliveryBroadcastReceiver.java
+++ app/src/main/java/receivers/DeliveryBroadcastReceiver.java
@@ -1,11 +1,15 @@
 package receivers;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
+import com.example.myapplication2.app.MainActivity;
 import com.example.myapplication2.app.R;
 
 /**
@@ -18,16 +22,35 @@ public class DeliveryBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(ACTION_SMS_DELIVERED)) {
+            CharSequence notification_title = "";
+            NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                    .setAutoCancel(true);
+
             switch (getResultCode()) {
                 case Activity.RESULT_OK:
-                    Toast.makeText(context, R.string.delivered_sms,
-                            Toast.LENGTH_SHORT).show();
+                    notification_title = context.getString(R.string.delivered_sms);
                     break;
                 case Activity.RESULT_CANCELED:
-                    Toast.makeText(context, R.string.not_delivered_sms,
-                            Toast.LENGTH_SHORT).show();
+                    notification_title = context.getString(R.string.not_delivered_sms);
                     break;
             }
+
+            mBuilder.setContentTitle(notification_title);
+            Intent activityIntent = new Intent(context, MainActivity.class);
+
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(activityIntent);
+            PendingIntent activityPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(activityPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(0, mBuilder.build());
         }
     }
 }
